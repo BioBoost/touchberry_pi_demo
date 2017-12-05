5a8721ba75e80c572c14929db02a2074ca4e501b
Nico De Witte <nico.dewitte@vives.be>
2017-12-05 12:48:57 +0100

    Starting demo project for touchberry that makes use of the package

---
+++ README.md
@@ -0,0 +1,12 @@
+# python-touchberrypi
+Python library for the Touchberry Pi shield for the Raspberry Pi
+
+## Install SmBus python package
+
+```shell
+sudo apt-get update && sudo apt-get install python-smbus
+```
+
+## Some info on SmBus methods
+
+[http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2](http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2)
---
+++ demo.py
@@ -0,0 +1,25 @@
+from touchberry_pi.touchberry_pi import TouchberryPi
+
+# def on_key_up(key):
+#     print("Key {} pressed".format(key))
+#
+#
+# touchberrypi.attach(on_key_up)
+#
+# print(touchberrypi.get_temperature())
+#
+# touchberrypi.set_all_leds("blue")
+#
+# touchberrypi.set_led(5, "red")
+
+
+shield = TouchberryPi()
+
+print("Temperature = " + str(shield.get_temperature()))
+
+def key_change_handler(key, state):
+    print("Key {} changed state to {}".format(key, state))
+
+shield.on_key_change(key_change_handler)
+
+print("Touch chip id = " + str(shield.get_touch().chip_id()))
